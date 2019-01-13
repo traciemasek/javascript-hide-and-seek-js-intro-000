908e1103ce4adaf9ca94f6716745755bfadd2a57
Tracie Masek <traciemasek@gmail.com>
2019-01-13 19:22:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('div#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n; 
+  }
+  
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+  
+}
